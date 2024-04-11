30e90220cb272a1ff4fb4ee3c79592ddbc03668b
itssmeden <deniseg.dg115@gmail.com>
2024-04-10 20:21:04 -0500

    bread

--- frontend/src/pages/our-group/denise.js
+++ frontend/src/pages/our-group/denise.js
@@ -1,1 +1,12 @@
-console.log("first code")
+import React from "react";
+const Denise = () => {
+    return (
+        <div>
+            <h1>Denise</h1>
+                <p></p>
+            <br/>
+        </div>
+    )
+};
+  
+export default Denise;
