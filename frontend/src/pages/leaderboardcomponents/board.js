13740d2c94c40f3765236010333b5dcef0b14753
Joseph Giap <josephgiap10@gmail.com>
2024-04-13 19:53:35 -0500

    added the leaderboard; buttons dont work

--- frontend/src/pages/leaderboardcomponents/board.js
+++ frontend/src/pages/leaderboardcomponents/board.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Profiles from './profiles.js';
 import {Leaderboard} from './databasetemp';
 
 export default function Board() {
 
+  const[period, setPeriod] = useState(0);
   const handleClick = (e) =>{
-    console.log(e.target);
+    setPeriod(e.target.dataset.id);
   }
 
   return (
-
     <div className = "board">
         <h1 className = "leaderboard">Leaderboard</h1>
 
@@ -18,7 +18,30 @@ export default function Board() {
             <button onClick={(handleClick)} data id = '30'>30 Days</button>
             <button onClick={(handleClick)} data id = '0'>All Time</button>
         </div>
-        <Profiles Leaderboard = {Leaderboard} ></Profiles>
+        <Profiles Leaderboard = {between(Leaderboard, period)} ></Profiles>
     </div>
   );
+}
+
+
+function between(data, between){
+  const today = new Date();
+  const previous = new Date(today);
+  previous.setDate(previous.getDate() - (between + 1));
+
+  let filter = data.filter(val => {
+      let userDate = new Date(val.dt);
+      if (between == 0) return val;
+      return previous <= userDate && today >= userDate;
+  })
+
+  // sort with asending order
+  return filter.sort((a, b) => {
+      if ( a.score === b.score){
+          return b.score - a.score;
+      } else{
+          return b.score - a.score;
+      }
+  })
+
 }
